42dade59642debfd4ff06a599f64815d60dbd02e
Bloaspiepken <philip.nijhof@icloud.com>
2016-05-01 14:34:48 +0000

    Modified Gemfile and controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,8 @@ class PagesController < ApplicationController
     
   end
   
+  def welcome
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#home'
+
+  root 'welcome#index'
+  
   get 'pages/about', to: 'pages#about'
+  
+  
   
 
   # Example of regular route:
